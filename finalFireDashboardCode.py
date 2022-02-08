f15f8cceb1cfe2efeeee1d75fd02e60060247ae5
Cat Schooley <93783235+catschooley@users.noreply.github.com>
2022-02-08 10:34:36 -0700

    Add csv and email
    Additional code for creating CSVs for endangered assets and emailing these CSVs to management

--- finalFireDashboardCode.py
+++ finalFireDashboardCode.py
@@ -40,6 +40,7 @@ amrLayerLocation = "https://services.arcgis.com/ZzrwjTRez6FJiOq4/arcgis/rest/ser
 
 utahFires = folderPath + "\\utahFires"
 fireBuffers = folderPath + "\\fireBuffers"
+fireBuffersNames = folderPath + "\\fireBuffersNames"
 coalLocations = folderPath + "\\coalLocations"
 mineralLocations = folderPath + "\\mineralLocations"
 oilGasLocations = folderPath + "\\oilGasLocations"
@@ -89,6 +90,14 @@ arcpy.analysis.MultipleRingBuffer(Input_Features = UtahFires,
                                   Distances = [0.1, 1, 5],
                                   Buffer_Unit = "Miles")
 
+# Stole this by using ArcPro and then copying the python command - could use Field Mapping object but with so many fields, this was easier
+arcpy.analysis.SpatialJoin(fireBuffers,
+                           testUtahFires,
+                           fireBuffersNames,
+                           "JOIN_ONE_TO_ONE",
+                           "KEEP_ALL",
+                           'distance "distance" true true false 19 Double 0 0,First,#,fireBuffers,distance,-1,-1;poly_IncidentName "Incident Name (Polygon)" true true false 50 Text 0 0,First,#,testingFireperimeters,poly_IncidentName,0,50;irwin_IncidentTypeCategory "Incident Type Category" true true false 2 Text 0 0,First,#,testingFireperimeters,irwin_IncidentTypeCategory,0,2;irwin_POOJurisdictionalAgency "POO Jurisdictional Agency" true true false 50 Text 0 0,First,#,testingFireperimeters,irwin_POOJurisdictionalAgency,0,50', "WITHIN_A_DISTANCE", "5 Miles", '')
+
 # Function for creating buffers
 def spatialJoinBuffers(targetFeatures, joinFeatures, outFeatureClass, matchOption):
     arcpy.analysis.SpatialJoin(target_features = targetFeatures,
@@ -97,16 +106,16 @@ def spatialJoinBuffers(targetFeatures, joinFeatures, outFeatureClass, matchOptio
                                match_option = matchOption)
 
 print("Joining coal mine locations to fire buffer layer")    
-spatialJoinBuffers(coalLocations, fireBuffers, coalJoined, "INTERSECT")
+spatialJoinBuffers(coalLocations, fireBuffersNames, coalJoined, "INTERSECT")
 
 print("Joining mineral mine locations to fire buffer layer")
-spatialJoinBuffers(mineralLocations, fireBuffers, mineralJoined, "INTERSECT")
+spatialJoinBuffers(mineralLocations, fireBuffersNames, mineralJoined, "INTERSECT")
 
 print("Joining oil and gas well locations to fire buffer layer")
-spatialJoinBuffers(oilGasLocations, fireBuffers, oilGasJoined, "INTERSECT")
+spatialJoinBuffers(oilGasLocations, fireBuffersNames, oilGasJoined, "INTERSECT")
 
 print("Joining abandoned mine locations to fire buffer layer")
-spatialJoinBuffers(amrLocations, fireBuffers, amrJoined, "INTERSECT")
+spatialJoinBuffers(amrLocations, fireBuffersNames, amrJoined, "INTERSECT")
 
 # ============================== Overwriting Hosted Feature Layers =====================================
 # Though overwriting the layers would be an option with the use of view layers as to not lose customizations like symbology and pop-ups, 
@@ -179,6 +188,35 @@ updateHosted(coalJoined, coalJoinedItemId)
 updateHosted(mineralJoined, mineralJoinedItemId)
 updateHosted(oilGasJoined, oilGasJoinedItemId)
 updateHosted(amrJoined, amrJoinedItemId)
+
+#============================ Create Table & Send Emails =======================================
+
+coalExcel = 'endangeredCoal.csv'
+mineralExcel = 'endangeredMineral.csv'
+oilGasExcel = 'endangeredOilGas.csv'
+amrExcel = 'endangeredAMR.csv'
+
+arcpy.conversion.TableToTable(oilGasJoined, excelFolder, oilGasExcel , "distance = .1")
+arcpy.conversion.TableToTable(coalJoined, excelFolder, coalExcel , "distance = .1")
+arcpy.conversion.TableToTable(mineralJoined, excelFolder, mineralExcel , "distance = .1")
+arcpy.conversion.TableToTable(amrJoined, excelFolder, amrExcel , "distance = .1")
+
+print("Sending Emails...")
+now = datetime.now()
+date = now.strftime("%m/%d/%Y")
+time = now.strftime("%I:%M:%S %p")
+reciever = ['email1@gmail.com', 'email2@gmail.com]
+body = f'Hello,\n\nThese are the DOGM assets that are within a fire perimeter. Please see attached file for a complete list.'
+yag = yagmail.SMTP("email", 'password')
+for recipient in reciever:
+    yag.send(
+        to=recipient,
+        subject='DOGM Assets in within Fire Perimeter',
+        attachments = [os.path.join(excelFolder, oilGasExcel)] #list the tables you want in the email
+        contents = body
+    )
+
+print('Emails sent successfully.')
 
 endTime = process_time()
 elapsedTime = round((endTime-startTime)/60, 2)
